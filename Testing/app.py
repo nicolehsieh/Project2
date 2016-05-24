243d3cb38960689eacecfbe05c9e6e5dccb3a180
Nicole Hsieh <nhsieh@cs.uoregon.edu>
2016-05-24 16:44:11 -0700

    made changes to app.py and leaf.html, currently having problem with displaying map with polyline, possibly due to points format

--- Testing/app.py
+++ Testing/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, g
 from werkzeug import secure_filename
 
 import sys
@@ -24,8 +24,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # allowed_file() function checks if extension is valid
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 # note - possible ambiguous routing in following two functions
@@ -38,28 +38,31 @@ def index():
 # and redirects the user to the URL for the uploaded file
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-	if request.method == 'POST':
-		# Get the name of the uploaded file
-		file = request.files['file']
+    if request.method == 'POST':
+        # Get the name of the uploaded file
+        file = request.files['file']
 
-		# Check if the file is one of the allowed types/extensions
-		if file and allowed_file(file.filename):
-			
-			# secure_filename() function secure a filename before storing it directly on the filesystem
-			# Make the filename safe, remove unsupported chars
-			filename = secure_filename(file.filename) 
+        # Check if the file is one of the allowed types/extensions
+        if file and allowed_file(file.filename):
+            
+            # secure_filename() function secure a filename before storing it directly on the filesystem
+            # Make the filename safe, remove unsupported chars
+            filename = secure_filename(file.filename) 
 
-			# Move the file form the temporal folder to the upload folder we setup
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			
-			# session store filename in cookies
-			# session['filename'] = filename
-			# session allows you to store information specific to a user from one request to the next. 
-			# This is implemented on top of cookies for you and signs the cookies cryptographically. 
+            # Move the file form the temporal folder to the upload folder we setup
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            points = load("static/uploads/" + filename, delta=100)
+            g.points= points
+            
+            # session store filename in cookies
+            # session['filename'] = filename
+            # session allows you to store information specific to a user from one request to the next. 
+            # This is implemented on top of cookies for you and signs the cookies cryptographically. 
 
-			# Redirect the user to the uploaded_file route, which 
-			# will basicaly show on the browser the uploaded file
-			return redirect(url_for('display', filename=filename))
+            # Redirect the user to the uploaded_file route, which 
+            # will basicaly show on the browser the uploaded file
+            return render_template("leaf.html")
+            
 
 # serves(handle) uploaded file
 # This route is expecting a parameter containing the name
@@ -68,16 +71,16 @@ def upload_file():
 # an image, that image is going to be show after the upload
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 # display gpx path on a map
 @app.route('/display', methods = ['GET','POST'])
 def display():
-	# retrieve file name from cookies session
-	# filename = session['filename']
-	filename = request.args.get('filename')
-	return render_template("leaf.html", name=filename)
+    # retrieve file name from cookies session
+    # filename = session['filename']
+    filename = request.args.get('filename')
+    return render_template("leaf.html")
 
 # set the secret key. 
 # app.secret_key = '\x81|fL\xec\xa2[\x12\xf0\x0f\xa8X'
@@ -85,44 +88,44 @@ def display():
 
 # from https://bitbucket.org/MichalYoung/enroute-saunter/src/be98484f0ae062ac8ed0b5f2829f4b78d16911dc/htbin/gpx_from_file.cgi?at=master&fileviewer=file-view-default
 def load(filename, delta = None):
-	"""
-	Return a list of (lat, lon) pairs retrieved from gpx file
-	
-	Args:
-		filename: path of gpx file
-		delta: If provided, simplify path to max deviation of delta meters.
-              	100 meters will generally simplify
-              	a RideWithGPS route radically.
-	Returns:
-        	A list of (lattitude, longitude) pairs.
-	"""
-	contents = open(filename, 'r', encoding="utf-8", errors="replace")
-	gpx = gpxpy.parse(contents)
-	if delta:
-        	gpx.simplify(delta)
-    	points = [ ]
-    	for track in gpx.tracks:
-        	for segment in track.segments:
-            		for point in segment.points:
-                		points.append( (point.latitude, point.longitude) )
-    	return points
+    """
+    Return a list of (lat, lon) pairs retrieved from gpx file
+    
+    Args:
+        filename: path of gpx file
+        delta: If provided, simplify path to max deviation of delta meters.
+                100 meters will generally simplify
+                a RideWithGPS route radically.
+    Returns:
+            A list of (lattitude, longitude) pairs.
+    """
+    contents = open(filename, 'r', encoding="utf-8", errors="replace")
+    gpx = gpxpy.parse(contents)
+    if delta:
+        gpx.simplify(delta)
+        points = [ ]
+        for track in gpx.tracks:
+            for segment in track.segments:
+                    for point in segment.points:
+                        points.append( [point.latitude, point.longitude] )
+        return points
 
 
 # # get gpx file
 # @app.route('/getgpx/<filename>', methods=['GET'])
 # def getgpx(filename):
-# 	f = open(filename)
-# 	result = ""
-# 	for line in f:
-# 		result += line
-# 	return result
+#   f = open(filename)
+#   result = ""
+#   for line in f:
+#       result += line
+#   return result
 
 # read file as python, and return as JSON, easier for manipulation, then 
 
 
 if __name__ == '__main__':
-	app.run(
-		debug=True, 
-		host='0.0.0.0', 
-		port=5000
-	)
+    app.run(
+        debug=True, 
+        host='0.0.0.0', 
+        port=5000
+    )
